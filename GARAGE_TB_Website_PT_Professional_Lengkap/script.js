039230c372290ebacfb098cb8365c95c4b807de6
akbarputrabahrudin-jpg <akbarputrabahrudin@gmail.com>
2026-09-16 18:29:36 +0700

    Delete GARAGE_TB_Website_PT_Professional_Lengkap directory

--- GARAGE_TB_Website_PT_Professional_Lengkap/script.js
+++
@@ -1,1 +0,0 @@
-const h=document.querySelector('.hamb'), n=document.querySelector('.nav');h.addEventListener('click',()=>n.classList.toggle('open'));document.querySelectorAll('nav a').forEach(a=>a.addEventListener('click',()=>n.classList.remove('open')));
